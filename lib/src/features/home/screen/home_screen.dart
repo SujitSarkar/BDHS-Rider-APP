04ee2d181d754e594913546b5dfbe4e75ebd570c
Sujit Sarkar <snigdho532@gmail.com>
2024-01-17 15:27:04 +0600

    UI update and bug fix

--- lib/src/features/home/screen/home_screen.dart
+++ lib/src/features/home/screen/home_screen.dart
@@ -44,10 +44,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           ///Header
           InkWell(
-            onTap: ()async{
-              AuthRepository authRepo = AuthRepository();
-              await authRepo.logout();
-            },
+            onTap: ()=> Navigator.pushNamed(context, AppRouter.profile),
             child: Stack(
               children: [
                 Image.asset(
